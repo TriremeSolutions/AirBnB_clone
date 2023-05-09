febddf7b3446831c1ad13c7efbd0aefa772d2630
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-09 22:09:44 +0300

    storage function partially added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,4 +20,16 @@ class FileStorage:
             new_dict[key] = value.to_dict()
         with open(self.__file_path, "w") as f:
             json.dump(new_dict, f)
-
+# TODO FIX THE RELOAD FUNCTION KEEP THROWING NameError: name 'models' is not defined
+    def reload(self):
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                data = json.load(f)
+                for key, value in data.items():
+                    cls_name, obj_id = key.split(".")
+                    obj_dict = value
+                    obj_dict["created_at"] = datetime.datetime.strptime(obj_dict["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+                    obj_dict["updated_at"] = datetime.datetime.strptime(obj_dict["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+                    module_name = "models.{}".format(cls_name.lower())
+                    cls = eval("{}.{}".format(module_name, cls_name))
+                    self.__objects[key] = cls(**obj_dict)
