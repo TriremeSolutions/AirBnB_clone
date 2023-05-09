75a34bb0f84b75888c46ff19ecca1ab85b3d6c4e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-09 16:24:05 +0300

    kwargs processed

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,16 @@ class BaseModel:
     """
     defines all common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
