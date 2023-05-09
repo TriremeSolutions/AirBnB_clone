50438d4e6b4252a76677419c2828fc7745c21ba3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-09 22:08:20 +0300

    file saver defined

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,3 +14,10 @@ class FileStorage:
         key = "{}.{}".format(type(obj).__name__, obj.id)
         self.__objects[key] = obj
 
+    def save(self):
+        new_dict = {}
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(new_dict, f)
+
