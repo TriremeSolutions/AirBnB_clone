3fbd6f84976ce20f745229a247f589b01da4ef21
TriremeSolutions <themellowread@gmail.com>
2023-05-10 11:59:13 -0700

    review class executable

---
+++ models/review.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines the Review class that inherits
+from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Defines a user review representation.
+
+    Attributes:
+        place_id (str): the place id of the review
+        representation
+        user_id (str): the user id.
+        text: the review text body
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
