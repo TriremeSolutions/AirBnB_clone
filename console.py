93c643538e6f04064cf75cf25a56966ba7414d7f
TriremeSolutions <themellowread@gmail.com>
2023-05-10 08:06:46 -0700

    implement console create command

--- console.py
+++ console.py
@@ -4,10 +4,15 @@ Console for AirBnB clone
 """
 
 import cmd
+from models import storage
+from models.base_model import BaseModel
+import json
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    def_class = ['BaseModel']
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -21,6 +26,22 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty line"""
         pass
+    
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel,
+        saves it (to the JSON file)
+        and prints the id
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.def_class:
+            print("** class doesn't exist **")
+        else:
+            n_dict = {'BaseModel': BaseModel}
+            my_model = n_dict[arg]()
+            print(my_model.id)
+            my_model.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
