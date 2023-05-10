d756509e47029fb461f9542cccbf4f7b5a7877c5
TriremeSolutions <themellowread@gmail.com>
2023-05-10 08:47:49 -0700

    test create show and destroy

--- console.py
+++ console.py
@@ -69,6 +69,35 @@ class HBNBCommand(cmd.Cmd):
                     return
             print("** no instance found **")
 
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id.
+        Change is saved into the JSON file
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        spl_arg = arg.split(' ')
+
+        if spl_arg[0] not in HBNBCommand.def_class:
+            print("** class doesn't exist **")
+        elif len(spl_arg) == 1:
+            print("** instance id missing **")
+        else:
+            obj_tot = storage.all()
+            for key, value in obj_tot.items():
+                name_obj = value.__class__.__name__
+                id_obj = value.id
+                if name_obj == spl_arg[0] and id_obj == spl_arg[1].strip('"'):
+                    del value
+                    del storage._FileStorage__objects[key]
+                    storage.save()
+                    return
+            print("** no instance found **")
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
